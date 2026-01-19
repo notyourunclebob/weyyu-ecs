1bd3ecd3c53694437c32a5a49960f419a9999679
notyourunclebob <jw.pcapp@gmail.com>
2026-01-19 11:50:39 -0400

    add employees/claims/categories to db seed

--- mongo/dbData.js
+++ mongo/dbData.js
@@ -1,2 +1,30 @@
-// Mongo shell commands here to seed MongoDB database
-// ...
+db.employees.drop();
+db.employees.insert([
+    {
+        "f_name": "Meredith",
+        "l_name": "Vickers",
+        "admin": true,
+
+    },
+]);
+
+db.claims.drop();
+db.claims.insert({
+
+});
+
+db.categories.drop();
+db.categories.insert([
+    {
+        "category": "Food"
+    },
+    {
+        "category": "Travel"
+    },
+    {
+        "category": "Lodging"
+    },
+    {
+        "category": "Medical"
+    },
+]);
