d402ea908340952c8a491e3747f87d634ad5d6d9
notyourunclebob <jw.pcapp@gmail.com>
2026-01-19 14:20:58 -0400

    add seed data for admin employee

--- mongo/dbData.js
+++ mongo/dbData.js
@@ -1,30 +1,61 @@
 db.employees.drop();
 db.employees.insert([
     {
-        "f_name": "Meredith",
-        "l_name": "Vickers",
+        "_id": "EC/#9549874308",
+        "firstName": "Meredith",
+        "lalsName": "Vickers",
         "admin": true,
-
+        // password is DavidSucks
+        "passWord": "$2a$12$mcre1cmb6I8PJYXU38qd/.staFwyZuR/SPOROtMwxjkBFvvzw.cNi"
     },
 ]);
 
 db.claims.drop();
-db.claims.insert({
+db.claims.insert([
+    {
+        "Status": "Open",
+        "employee_id": "EC/#9549874308",
+        "receipt": "489104590124890.png",
+        "description": "Purchase of Weyland Yutani personal medical pod (experimental)",
+        "category": {
+            "name": "Medical",
+            "faceHugger": false
+        }
+        
 
-});
+    },
+]);
 
 db.categories.drop();
 db.categories.insert([
     {
-        "category": "Food"
+        "name": "Food",
+        "allowChange": false,
+        "requirements": [
+            {"requirement": "none", "dataType": "none"}
+        ]
     },
     {
-        "category": "Travel"
+        "name": "Travel",
+        "allowChange": false,
+        "requirements": [
+            {"requirement": "locationStart", "dataType": "string"},
+            {"requirement": "locationEnd", "dataType": "string"},
+            {"requirement": "distanceKm", "dataType": "number"},
+        ]
     },
     {
-        "category": "Lodging"
+        "name": "Food",
+        "allowChange": false,
+        "requirements": [
+            {"requirement": "none"}
+        ]
     },
     {
-        "category": "Medical"
+        "name": "Food",
+        "allowChange": false,
+        "requirements": [
+            {"requirement": "none"}
+        ]
     },
 ]);
