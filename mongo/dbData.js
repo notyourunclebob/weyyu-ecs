264271a32efbe65e9f54a5b794509a8d8e8cf57a
notyourunclebob <jw.pcapp@gmail.com>
2026-01-19 14:59:43 -0400

    add more seed data to db for testing
    one admin and employee/ two claims/ four categories

--- mongo/dbData.js
+++ mongo/dbData.js
@@ -6,14 +6,22 @@ db.employees.insert([
         "lalsName": "Vickers",
         "admin": true,
         // password is DavidSucks
-        "passWord": "$2a$12$mcre1cmb6I8PJYXU38qd/.staFwyZuR/SPOROtMwxjkBFvvzw.cNi"
+        "password": "$2a$12$mcre1cmb6I8PJYXU38qd/.staFwyZuR/SPOROtMwxjkBFvvzw.cNi"
     },
+    {
+        "_id": "EC/#4861258765",
+        "firstName": "Idris",
+        "lastName": "Janek",
+        "admin": false,
+        // password is SteveStills
+        "password": "$2a$12$suSiFUYSpmfel4h5YGvzgeUmj3lE0CtAP2n7bV1B1mwsIckQXYi62"
+    }
 ]);
 
 db.claims.drop();
 db.claims.insert([
-    {
-        "Status": "Open",
+    { 
+        "Status": "open",
         "employee_id": "EC/#9549874308",
         "receipt": "489104590124890.png",
         "description": "Purchase of Weyland Yutani personal medical pod (experimental)",
@@ -21,41 +29,53 @@ db.claims.insert([
             "name": "Medical",
             "faceHugger": false
         }
-        
-
     },
+    {
+        "Sataus": "open",
+        "employee_id": "EC/#4861258765",
+        "receipt": "582918453217090.png",
+        "description": "Visit to US headquarters",
+        "category": {
+            "name": "Travel",
+            "locationStart": "Sea of Tranquility, Moon",
+            "locationEnd": "San Francisco, California",
+            "distanceKm": 363104
+        }
+    }
 ]);
 
 db.categories.drop();
 db.categories.insert([
+    // categories are used as a reference to determine reqired fields when making a claim
     {
         "name": "Food",
         "allowChange": false,
+        // This could also be hard coded to make things easier.
         "requirements": [
-            {"requirement": "none", "dataType": "none"}
+            {"requirement": "none"}
         ]
     },
     {
         "name": "Travel",
         "allowChange": false,
         "requirements": [
-            {"requirement": "locationStart", "dataType": "string"},
-            {"requirement": "locationEnd", "dataType": "string"},
-            {"requirement": "distanceKm", "dataType": "number"},
+            {"requirement": "locationStart"},
+            {"requirement": "locationEnd"},
+            {"requirement": "distanceKm"},
         ]
     },
     {
-        "name": "Food",
+        "name": "Lodging",
         "allowChange": false,
         "requirements": [
             {"requirement": "none"}
         ]
     },
     {
-        "name": "Food",
+        "name": "Medical",
         "allowChange": false,
         "requirements": [
-            {"requirement": "none"}
+            {"requirement": "facehugger"}
         ]
     },
 ]);
