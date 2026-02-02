ed19e7802ca854d4b5f5cad071ad53e4623a0160
DrewMac2790 <drew.maceachern271@gmail.com>
2026-02-02 12:52:14 -0400

    add employye dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Dashboard from "../components/Dashboard";
+
+export default function Home() {
+    return (
+        <div className="w-screen h-screen bg-gray-400">
+            <Header />
+            <Dashboard />
+
+        </div>
+    );
+}
