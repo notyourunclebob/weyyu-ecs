6f99f30dc0bb1988ecbff65c9f4ca82646adf15f
notyourunclebob <jw.pcapp@gmail.com>
2026-02-04 16:45:31 -0400

    add update employee
    needs to use _id from mongo. will update id variables to reflect this

--- src/tools/EmployeeManager.ts
+++ src/tools/EmployeeManager.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from "mongodb";
+import { Collection, MongoClient, ObjectId, UpdateResult } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import sanitize from "sanitize-html";
 import { Employee } from "./employee.model";
@@ -110,4 +110,53 @@ export async function getEmployees() {
         },
         { status: 200 }
     );
+}
+
+// updates an existing employee
+export async function updateEmployee(request:NextRequest, id:string) {
+
+    let mongoClient: MongoClient = new MongoClient(URL);
+
+    try {
+        await mongoClient.connect();
+
+        let employeeId:ObjectId = new ObjectId(sanitize(id));
+
+        const body:any = await request.json();
+
+        body.firstName = sanitize(body.firstName);
+        body.lastName = sanitize(body.lastName);
+        // ***need to add a way to check and change passwords***
+        // body.password = sanitize(body.password);
+
+        let employeeCollection: Collection<Employee> = mongoClient.db(DB_NAME).collection<Employee>(COLLECTION_EMPLOYEES);
+        let selector:Object = { "_id": employeeId };
+        let newValues:Object = { $set: { 
+            firstName: body.firstName, 
+            lastName: body.lastName, 
+            admin: body.admin } };
+        let result:UpdateResult = await employeeCollection.updateOne(selector, newValues);
+
+        if (result.matchedCount <= 0 ) {
+            return NextResponse.json(
+                { error: "Employee id doesn't exist" },
+                { status: 404, statusText: "Employee id doesn't exist" }
+            );
+        } else {
+            return NextResponse.json(
+                {
+                    message: "Employee updated",
+                    result
+                },
+                { status: 200 }
+            );
+        }
+    } catch (error:any) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        );
+    } finally {
+        mongoClient.close();
+    }
 }
