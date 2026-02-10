33ba30d7d40071d31ab1a8d391fd4203f068f6d9
notyourunclebob <jw.pcapp@gmail.com>
2026-02-10 13:12:40 -0400

    add module data types

---
+++ src/tools/next-auth.d.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+
+// this extends nextauth's types to include the ecs user data
+declare module "next-auth" {
+    interface User {
+        admin?: boolean;
+    }
+    interface Session {
+        user: {
+            id: string;
+            employeeId: string;
+            firstName: string;
+            lastName: string;
+            admin: boolean;
+        }
+    }
+}
+
+// this is the authentication token data
+declare module "next-auth/jwt" {
+    interface JWT {
+        andmin?: boolean;
+        id?: string;
+    }
+}
