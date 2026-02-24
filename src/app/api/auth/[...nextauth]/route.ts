63b22ed2d8c1d81215d0ca751a31412858429395
DrewMac2790 <drew.maceachern271@gmail.com>
2026-02-24 13:53:12 -0400

    add console.log for testing

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+console.log("route.ts loaded");
+console.log("authOptions:", authOptions);
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
